a1d96f6bb60e9ddd2e7350a8b1127ebb3fc94b9a
Bishal Kumar Ghosh <81809899+bISHAL-2001@users.noreply.github.com>
2022-01-14 12:44:27 +0530

    Q6.FindLargestSubarrayHavingEqualNumber_of_0s&1s.c

---
+++ Find_the_largest_subarray_having_an_equal_number_of_0s_and_1s.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print(int *arr, int l, int u)
+{
+    for(int i = l; i<=u; i++)
+        printf("%d ", arr[i]);
+}
+
+void check(int *arr, int size) {
+    int u, l;
+    for (int i = 0; i < size;) {
+        int j = i, zeroes = 0, ones = 0;
+        while (j < size) {
+            if(arr[j] == 0)
+                zeroes += 1;
+            else if(arr[j] == 1)
+                ones += 1;
+            if (zeroes == ones && j-i >= u-l) {
+                l = i;
+                u = j;
+            }
+            j++;
+        }
+        i++;
+    }
+    if(u-l != 0) {
+        printf("The SubArray with maximum length with equal number of zeroes and ones = %d,\nSubarray is/are :->", u+1 - l);
+        print(arr, l, u);
+    }
+}
+
+int main(void)
+{
+    int l;
+    printf("Enter the expected length of the binary array:");
+    scanf("%d", &l);
+
+    int *arr = (int*)calloc(l, sizeof(int));
+    printf("Enter %d binary digits(0s and 1s):", l);
+
+    for(int i = 0; i < l;) {
+        int temp;
+        scanf("%d", &temp);
+        if(temp == 0 || temp == 1)
+            arr[i++] = temp;
+        else
+            printf("Wrong Input! Enter a binary digit: ");
+    }
+
+    check(arr, l);
+
+    arr = NULL;
+    free(arr);
+}
